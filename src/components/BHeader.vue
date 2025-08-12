2cb3ce4a642267d38fa1333dd0374c90f8a0b283
MMintiaa <zwuu0129@student.monash.edu>
2025-08-12 17:49:13 +0800

    Lab 12

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -5,12 +5,14 @@
     <header class="d-flex justify-content-center py-3">
       <ul class="nav nav-pills">
         <li class="nav-item">
-          <router-link to="/" class="nav-link" active-class="active" aria-current="page"
-            >Home (Week 5)</router-link
-          >
+          <router-link :to="{ name: 'Home' }" class="nav-link" active-class="active" aria-current="page">
+            Home (Week 5)
+          </router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/about" class="nav-link" active-class="active">About</router-link>
+          <router-link :to="{ name: 'About' }" class="nav-link" active-class="active">
+            About
+          </router-link>
         </li>
       </ul>
     </header>
